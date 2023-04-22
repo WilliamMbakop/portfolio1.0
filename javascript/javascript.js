165b90cd67f1e967a32abe21c36a4cc27d0a098a
William Mbakop <william.mbakop@oclock.school>
2023-04-22 13:57:30 +0200

    ajout animation title + photo profil

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,3 +5,52 @@ containerLightNight.addEventListener('click', function(){
     let body = document.querySelector('body');
     body.classList.toggle("body--darkMode");
 });
+
+// ********** CHANGEMENT DU TITLE ********** //
+let navbarItems = document.querySelectorAll('.navbar__items');
+let title = document.querySelector('title');
+
+for (const navbarItem of navbarItems) {
+
+    switch (navbarItem.textContent) {
+        case 'Présentation':
+            navbarItem.addEventListener('click',function(){
+                title.textContent='Présentation';
+            })            
+            break;
+        case 'Technologies':
+            navbarItem.addEventListener('click',function(){
+                title.textContent='Technologies';
+            })            
+            break;
+         case 'Projets':
+            navbarItem.addEventListener('click',function(){
+                title.textContent='Projets';
+            })            
+            break;
+         case 'Contact':
+            navbarItem.addEventListener('click',function(){
+                title.textContent='Contact';
+            })            
+            break;        
+    
+        default:
+            navbarItem.addEventListener('click',function(){
+                title.textContent='Portfolio William Mbakop';
+            })            
+            break;
+    }
+}
+
+// ********** ANIMATION PHOTO PROFIL ********** //
+
+let presentationPicture = document.querySelector('.presentation__picture');
+
+presentationPicture.addEventListener('mouseenter', function(){
+
+    if(presentationPicture.src == "images/photo_profil.jpg"){
+        presentationPicture.src="images/picture-logo.png";        
+    }else{
+        presentationPicture.src="images/photo_profil.jpg";
+    }
+});
